10d08035b8b1a599d5f13e358d533d898a217a7d
diamondburned <diamond@arikawa-hi.me>
2023-12-05 11:58:33 -0800

    Implement seeing and saving move history

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,10 +1,14 @@
-import { Game, Outcome, Player } from "/game.ts";
+import { Game, Move, Outcome, Player } from "/game.ts";
 import { HumanPlayer } from "/frontend/player_human.ts";
 import { AIPlayer } from "/frontend/player_ai.ts";
+import * as templates from "/frontend/templates.ts";
 import * as ai from "/ai/ai.ts";
 
-const modelName = "train-1701665330248.model";
+const modelName = "train-1701739095888.model";
 const modelPath = `${window.location.origin}/ai/models/${modelName}`;
+
+const moveHistoryElem = document.getElementById("move-history");
+const saveHistoryButton = document.getElementById("save-history") as HTMLButtonElement;
 
 const turnElem = document.getElementById("turn");
 const vsResult = document.querySelector("#vs .result");
@@ -23,10 +27,47 @@ const player2: Player = new AIPlayer(
 
 const game = new Game(player1, player2);
 
+saveHistoryButton.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  const movesJSON = JSON.stringify(game.moves);
+  const blob = new Blob([movesJSON], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `game-${Date.now()}.json`;
+  a.click();
+  URL.revokeObjectURL(url);
+});
+
+const moveIcons: Record<Move, templates.TemplateID> = {
+  [Move.Reload]: "iconReload",
+  [Move.Shoot]: "iconShoot",
+  [Move.Block]: "iconBlock",
+  [Move.TakeOutKnife]: "iconTakeOutKnife",
+  [Move.Stab]: "iconStab",
+};
+
 let outcome: Outcome;
 do {
   turnElem.textContent = `${game.turn}`;
   outcome = await game.play();
+
+  if (game.moves.length == 1) {
+    moveHistoryElem.innerHTML = "";
+    saveHistoryButton.disabled = false;
+  }
+
+  const [p1Move, p2Move] = game.moves[game.moves.length - 1];
+
+  const p1MoveElem = templates.clone(moveIcons[p1Move]);
+  const p2MoveElem = templates.clone(moveIcons[p2Move]);
+
+  const moveItem = document.createElement("div");
+  moveItem.classList.add("move");
+  moveItem.appendChild(p1MoveElem);
+  moveItem.appendChild(p2MoveElem);
+
+  moveHistoryElem.appendChild(moveItem);
 } while (outcome == Outcome.Draw);
 
 switch (outcome) {
--- frontend/templates.ts
+++ frontend/templates.ts
@@ -1,19 +1,31 @@
+export const templates = {
+  buttonReload: getTemplateWithID("button-reload"),
+  buttonShoot: getTemplateWithID("button-shoot"),
+  buttonBlock: getTemplateWithID("button-block"),
+  buttonTakeOutKnife: getTemplateWithID("button-take-out-knife"),
+  buttonStab: getTemplateWithID("button-stab"),
+
+  iconReload: getTemplateWithID("reload-icon"),
+  iconShoot: getTemplateWithID("shoot-icon"),
+  iconBlock: getTemplateWithID("block-icon"),
+  iconTakeOutKnife: getTemplateWithID("take-out-knife-icon"),
+  iconStab: getTemplateWithID("stab-icon"),
+};
+
+export type TemplateID = keyof typeof templates;
+
+export function clone<T extends HTMLElement>(id: TemplateID): T {
+  const t = templates[id] as HTMLTemplateElement;
+  if (!t) {
+    throw new Error(`Could not find template with ID ${id as string}`);
+  }
+  const childNodes = [...t.content.cloneNode(true).childNodes];
+  return childNodes.find((e) => e instanceof HTMLElement) as T;
+}
+
 export default {
-  _t: {
-    buttonReload: getTemplateWithID("button-reload"),
-    buttonShoot: getTemplateWithID("button-shoot"),
-    buttonBlock: getTemplateWithID("button-block"),
-    buttonTakeOutKnife: getTemplateWithID("button-take-out-knife"),
-    buttonStab: getTemplateWithID("button-stab"),
-  },
-  clone<T extends HTMLElement>(id: keyof typeof this._t): T {
-    const t = this._t[id] as HTMLTemplateElement;
-    if (!t) {
-      throw new Error(`Could not find template with ID ${id as string}`);
-    }
-    const childNodes = [...t.content.cloneNode(true).childNodes];
-    return childNodes.find((e) => e instanceof HTMLElement) as T;
-  },
+  _t: templates,
+  clone,
 };
 
 function getTemplateWithID(id: string): HTMLTemplateElement {
